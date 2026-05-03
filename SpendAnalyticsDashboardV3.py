bb424d182d05a7875fdb6db04e549033595926b5
Ranjani <48840708+ranjbanu@users.noreply.github.com>
2026-05-03 21:23:32 +0530

    Update SpendAnalyticsDashboardV3.py

--- SpendAnalyticsDashboardV3.py
+++ SpendAnalyticsDashboardV3.py
@@ -414,7 +414,7 @@ def forecast_by_category(
             "Month": pd.period_range(
                 series.index.max() + 1, periods=horizon, freq="M"
             ),
-            "Forecast (in million)": np.zeros(horizon)
+            "Forecast": np.zeros(horizon)
         })
 
     model = fit_sarima(series, season)
@@ -429,7 +429,7 @@ def forecast_by_category(
         "Month": pd.period_range(
             series.index.max() + 1, periods=horizon, freq="M"
         ),
-        "Forecast": forecast / 1000000
+        "Forecast(in million)": round(forecast / 1000000,2)
     })
 
 # ---------------------------
